274587a6bdfaf608be42c5388f85f71df04587bb
Simondbf <64691542+Simondbf@users.noreply.github.com>
2026-08-25 20:16:48 +0200

    Update service worker cache version and files

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Service worker de CinéPrivé.
 // A incrementer a chaque deploiement touchant aux fichiers du site.
-const CACHE_NAME = 'cineprive-v1';
-const PRECACHE = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE_NAME = 'cineprive-v2';
+const PRECACHE = ['/', '/index.html', '/manifest.json', '/favicon.svg', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
